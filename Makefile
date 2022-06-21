cae953984003e833f735dfdd52ef57542cda84da
chris bao <baoqger@aliyun.com>
2022-06-21 10:21:10 +0800

    add content

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 basiccaller: linked-list.c basiccaller.c
 	gcc linked-list.c basiccaller.c -Wall -o $@
+count: linked-list.c count.c
+	gcc linked-list.c count.c -Wall -g  -o $@
 clean:
 	rm basiccaller
 	
---
+++ count.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "linked-list.h"
+/*
+ * Given a list and an int, return the number of times that int occurs in the list
+ * */
+int Count(struct node *head, int searchFor) {
+    int counter = 0;
+    struct node *current = NULL;
+    current = head;
+    while(current != NULL) {
+        if (current->data == searchFor) {
+           counter++; 
+        }
+        current = current->next;
+    }
+    return counter;
+}
+
+int main() {
+    int i;
+    struct node *list;
+    BuildOneTwoThree(&list);
+    i = Count(list, 1);
+    printf("i = %d\n", i);
+}
