0f27085243d3208a2c712b7b678fee50e12a33f5
chris bao <baoqger@aliyun.com>
2022-06-29 10:29:12 +0800

    add content

--- josephus.c
+++ josephus.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 #include "linked-list.h"
 
+int josephus_dp_2(int n, int k) {
+    if (n == 1) {
+        return 1;
+    }
+    return (josephus_dp_2(n - 1, k) + k -1)%n + 1;
+}
+
+int josephus_dp(int n ,int k) {
+    //f(n,k) = (f(n-1, k) + k -1)%n + 1; 
+    int res = 1;
+    for (int i = 2; i <= n; i++) {
+        res = (res + k - 1)%i + 1;
+    }
+    return res;
+}
+
+
 int josephus(struct node *head, int n, int k) {
     int len = n;
     int step = 1;
@@ -35,6 +52,7 @@ int main() {
     Push(&head, 1);
     struct node *tail = head;
     int n = 9;
+    int k = 3;
     for(int i = 2; i <= n; i++) {
         Push(&tail->next, i);
         tail = tail->next;
@@ -43,12 +61,16 @@ int main() {
 
     // ListPrint(head);
     
-    printf("survivor of j(9,3): %d\n", josephus(head, 9, 3));
+    printf("survivor of j(%d,%d): %d\n", n, k, josephus(head, n, 3));
 
 
     // printf("survivor of j(8,2): %d\n", josephus(head, 8, 2));
     // printf("survivor of j(9,2): %d\n", josephus(head, 9, 2));
     // printf("survivor of j(15,2): %d\n", josephus(head, 15, 2));
+
+    printf("j(%d,%d) result of dp: %d\n",n, k ,josephus_dp(n ,k));
+    printf("j(%d,%d) result of dp: %d\n",n, k ,josephus_dp_2(n ,k));
+
 
     return 0;
 }
